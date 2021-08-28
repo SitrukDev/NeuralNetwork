88d6dc3cacd45113e2aa539f1314dda77b781af7
ofir sitruk <odsd1402@gmail.com>
2021-08-29 02:05:12 +0300

    add(Layer): adding backPropagation algorithm

--- NeuralNetworkProject/src/main/java/layers/Layer.java
+++ NeuralNetworkProject/src/main/java/layers/Layer.java
@@ -12,19 +12,19 @@ public abstract class Layer {
 	private SimpleMatrix weightsGrad;
 	private SimpleMatrix biasGrad;
 	private SimpleMatrix inputs;
-	
+
 	private Activation activation;
-	
-	public Layer(int inputs, int outputs) { 
-		this.changeBias(new SimpleMatrix(inputs, 1));
+
+	public Layer(int inputs, int outputs) {
+		this.changeBias(new SimpleMatrix(inputs, 1)); // TODO random
 		this.changeBiasGrad(new SimpleMatrix(inputs, 1));
-		this.changeInputs(new SimpleMatrix(inputs, outputs));
-		this.changeWeights(new SimpleMatrix(inputs, outputs));
-		this.changeWeightsGrad(new SimpleMatrix(inputs, outputs));
-		
+		this.changeInputs(new SimpleMatrix(inputs, 1));
+		this.changeWeights(new SimpleMatrix(outputs, inputs)); // TODO random
+		this.changeWeightsGrad(new SimpleMatrix(outputs, inputs));
+
 		this.changeActivation(new Relu());
 	}
-	
+
 	public int inputFeatures() {
 		return this.weights.numRows();
 	}
@@ -76,28 +76,48 @@ public abstract class Layer {
 	public Activation activation() {
 		return this.activation;
 	}
-	
+
 	public void changeActivation(Activation activation) {
 		this.activation = activation;
 	}
-	
+
 	public SimpleMatrix feedforward(SimpleMatrix inputs) {
 		this.changeInputs(inputs);
-		
+
 		SimpleMatrix outputs = this.weights().mult(inputs);
 
+		outputs.plus(this.bias());
+
 		for (int outputRow = 0; outputRow < outputs.numRows(); outputRow++) {
-			outputs.set(outputRow,  0, outputs.get(outputRow, 0) + this.bias().get(outputRow, 0));
-			
-			outputs.set(outputRow,  0, this.activation().activationFunction(outputs.get(outputRow, 0)));
+			outputs.set(outputRow, 0, this.activation().activationFunction(outputs.get(outputRow, 0)));
 		}
-		
+
 		return outputs;
 	}
-	
-	 public SimpleMatrix backPropagation(SimpleMatrix errors, int learningRate) { 
-		 SimpleMatrix outputs = this.feedforward(this.inputs());
-		 
-		 
-	 }
+
+	public SimpleMatrix backPropagation(SimpleMatrix errors, int learningRate) {
+		SimpleMatrix outputs = this.feedforward(this.inputs());
+
+		for (int outputRow = 0; outputRow < outputs.numRows(); outputRow++) {
+			outputs.set(outputRow, 0, this.activation().inverse(outputs.get(outputRow, 0)));
+		}
+
+		SimpleMatrix gradients = outputs.mult(errors); // Maybe elementWise multiplication!!!
+
+		for (int index = 0; index < gradients.getNumElements(); index++) {
+			gradients.set(index, gradients.get(index) * learningRate);
+		}
+
+		this.changeBiasGrad(gradients);
+
+		SimpleMatrix inputsTransposed = gradients.transpose();
+
+		this.changeWeightsGrad(gradients.mult(inputsTransposed));
+
+		this.changeBias(this.bias().plus(gradients));
+		this.changeWeights(this.weightsGrad());
+
+		// SimpleMatrix previousLayerErrors = 
+		return this.weights().transpose().mult(errors);
+	}
 }
